849def305e81c016f5232bd59d44b4628fc28e35
yashdivya5 <121162537+yashdivya5@users.noreply.github.com>
2024-01-05 00:11:20 +0530

    Merge branch 'main' of github.com:udit2002-c/p2p

--- documents.js
+++ documents.js
@@ -1,7 +1,4 @@
 const usersschema = require("./datamodel/usersschema");
 const mongoose = require("mongoose");
 const mongooseSchema = new mongoose.Schema({ name: String });
-const MyModel = mongoose.model("Test", new Schema(usersschema));
-
-// module.exports = mongoose.model("MyModel", usersschema);
-
+const MyModel = mongoose.model("Test", new mongoose.Schema(usersschema));
